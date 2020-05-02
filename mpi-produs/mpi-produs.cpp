98b88c29ec424d37312526d2466d0dfe662b53a2
PatiVioleta <48491987+PatiVioleta@users.noreply.github.com>
2020-05-02 11:24:01 +0300

    Renames

--- mpi-produs/mpi-produs.cpp
+++ mpi-produs/mpi-produs.cpp
@@ -1,173 +1,296 @@
 #include "pch.h"
+#include "mpi.h"
 #include <string>
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <map>
 #include <chrono>
 
 #define CAST std::chrono::duration_cast<std::chrono::nanoseconds>
+#define TIP_TIMP std::chrono::steady_clock::time_point
 
-struct MatriceCSR {
-	long long nr_linii, nr_coloane;
-	std::vector<long long> V, COL_INDEX, ROW_INDEX;
-} Matrice1, Matrice2;
-
-void citireMatrice(std::ifstream &MatriceFile, MatriceCSR &Matrice) {
-	MatriceFile >> Matrice.nr_linii;
-	MatriceFile >> Matrice.nr_coloane;
-
-	Matrice.ROW_INDEX.push_back(0);
-
-	for (long long i = 0; i < Matrice.nr_linii; i++) {
-		for (long long j = 0; j < Matrice.nr_coloane; j++) {
-			long long x;
-			MatriceFile >> x;
-			if (x != 0) {
-				Matrice.V.push_back(x);
-				Matrice.COL_INDEX.push_back(j);
-			}
-		}
-		Matrice.ROW_INDEX.push_back(Matrice.V.size());
-	}
-}
-
-void scriereMatrice(std::ofstream &MatriceFile, std::vector< std::vector<long long> > &Matrice) {
-	MatriceFile << Matrice1.nr_linii << " " << Matrice2.nr_linii << std::endl;
-	for (long long i = 0; i < Matrice1.nr_linii; i++) {
-		for (long long j = 0; j < Matrice2.nr_linii; j++) {
-			MatriceFile << Matrice[i][j] << " ";
-		}
-		MatriceFile << std::endl;
-	}
-}
-
-//calculeaza transpusa in format CSR a unei matrice in format CSR
-MatriceCSR transpusaCSR(MatriceCSR &Matrice) {
-	MatriceCSR Rezultat;
-
-	Rezultat.nr_linii = Matrice.nr_coloane;
-	Rezultat.nr_coloane = Matrice.nr_linii;
-	Rezultat.ROW_INDEX.push_back(0);
-
-	for (long long j = 0; j < Matrice.nr_coloane; j++) {
-		for (long long i = 0; i < Matrice.nr_coloane; i++) {
-			if (Matrice.COL_INDEX.at(i) == j) {
-				Rezultat.V.push_back(Matrice.V.at(i));
-
-				long long k = 0;
-				while (Matrice.ROW_INDEX.at(k) <= i)
-					k++;
-				Rezultat.COL_INDEX.push_back(k - 1);
-			}
-		}
-		Rezultat.ROW_INDEX.push_back(Rezultat.V.size());
-	}
-
-	return Rezultat;
-}
-
-//returneaza subsecventa din vector incepand de la indexul x si terminand la indexul y-1
-std::vector<long long> subsecventa(std::vector<long long> vector, long long x, long long y) {
-	std::vector<long long>::const_iterator first = vector.begin() + x;
-	std::vector<long long>::const_iterator last = vector.begin() + y;
-	std::vector<long long> rezultat(first, last);
-	return rezultat;
-}
-
-//returneaza elementele comune a doi vectori (ca valoare) si indecsii corespunzatori valorilor comune pentru fiecare dintre cei doi vectori (idx1 si idx2)
-std::vector<long long> intersectie(std::vector<long long> vector1, std::vector<long long> vector2, std::vector<long long> &idx1, std::vector<long long> &idx2) {
-	std::vector<long long> rezultat;
-	long long curent1 = 0, curent2 = 0;
-
-	while (curent1 < vector1.size() && curent2 < vector2.size()) {
-		long long left = vector1[curent1];
-		long long right = vector2[curent2];
-
-		if (left == right) {
-			rezultat.push_back(right);
-			idx1.push_back(curent1);
-			idx2.push_back(curent2);
-
-			while (curent1 < vector1.size() && vector1[curent1] == left)
-				curent1++;
-			while (curent2 < vector2.size() && vector2[curent2] == right)
-				curent2++;
-			continue;
-		}
-
-		if (left < right) {
-			while (curent1 < vector1.size() && vector1[curent1] == left)
-				curent1++;
-		}
-		else 
-			while (curent2 < vector2.size() && vector2[curent2] == right)
-				curent2++;
-	}
-	return rezultat;
-}
-
-std::vector< std::vector<long long> > produsCSR(MatriceCSR &Matrice1, MatriceCSR &Matrice2) {
-
-	//Pregatire matrice rezultat
-	std::vector< std::vector<long long> > A;
-	A.resize(Matrice1.nr_linii);
-	for (long long i = 0; i < Matrice1.nr_linii; ++i)
-	{
-		A[i].resize(Matrice2.nr_linii);
-	}
-
-	//Calculare produs
-	long long linii_matrice1 = Matrice1.nr_linii;
-	long long linii_matrice2 = Matrice2.nr_linii;
-	for (long long linie_curenta_matrice1 = 1; linie_curenta_matrice1 <= linii_matrice1; linie_curenta_matrice1++) {
-
-		std::vector<long long> COL_INDEX_1 = subsecventa(Matrice1.COL_INDEX, Matrice1.ROW_INDEX[linie_curenta_matrice1 - 1], Matrice1.ROW_INDEX[linie_curenta_matrice1]);
-		std::vector<long long> V_1 = subsecventa(Matrice1.V, Matrice1.ROW_INDEX[linie_curenta_matrice1 - 1], Matrice1.ROW_INDEX[linie_curenta_matrice1]);
-
-		for (long long linie_curenta_matrice2 = 1; linie_curenta_matrice2 <= linii_matrice2; linie_curenta_matrice2++) {
-			A[linie_curenta_matrice1-1][linie_curenta_matrice2-1] = 0;
-			
-			std::vector<long long> COL_INDEX_2 = subsecventa(Matrice2.COL_INDEX, Matrice2.ROW_INDEX[linie_curenta_matrice2 - 1], Matrice2.ROW_INDEX[linie_curenta_matrice2]);
-			std::vector<long long> V_2 = subsecventa(Matrice2.V, Matrice2.ROW_INDEX[linie_curenta_matrice2 - 1], Matrice2.ROW_INDEX[linie_curenta_matrice2]);
-
-			std::vector<long long> idx1;
-			std::vector<long long> idx2;
-
-			std::vector<long long> COL_INDEX_BOTH = intersectie(COL_INDEX_1, COL_INDEX_2, idx1, idx2);
-			for (long long k = 0; k < COL_INDEX_BOTH.size(); k++) {
-				A[linie_curenta_matrice1-1][linie_curenta_matrice2-1] += V_1[idx1[k]] * V_2[idx2[k]];
-			}
-		}
-	}
-	return A;
-}
+using namespace std;
 
 int main()
 {
-	std::string prefix_fisier = "C:\\Users\\pati\\Desktop\\UNIV\\SEM6\\PP\\proiect\\data\\generat10";
+	//variabile care nu sunt partajate
+	int nrprocese, rank, flag = 0;
+	MPI_Status status;
+	MPI_Request request;
+	char *segment_number1, *segment_number2, *number1, *number2;
+	int *sendcounts, *displs;
+	TIP_TIMP timp_start, timp_end;
 
-	std::ifstream Matrice1File(prefix_fisier + "A.txt");
-	std::ifstream Matrice2File(prefix_fisier + "B.txt");
-	std::ofstream RezultatFile(prefix_fisier + "C.txt");
+	number1 = (char*)malloc(0);
+	number2 = (char*)malloc(0);
+	sendcounts = (int*)malloc(0);
+	displs = (int*)malloc(0);
+	int rem = 0, sum = 0, len_segm = 0;
 
-	citireMatrice(Matrice1File, Matrice1);
-	citireMatrice(Matrice2File, Matrice2);
+	//Initialize the MPI environment
+	int rc = MPI_Init(NULL, NULL);
+	if (rc != MPI_SUCCESS) {
+		cout << "Err MPI Init";
+		//MPI_COMM_WORLD comunicator predefinit
+		MPI_Abort(MPI_COMM_WORLD, rc);
+	}
 
-	Matrice2 = transpusaCSR(Matrice2);
+	MPI_Comm_size(MPI_COMM_WORLD, &nrprocese);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	////------------------------------------------------------------------
-	auto start = std::chrono::high_resolution_clock::now();
+	if (rank == 0) {
+		//-----------------------------------------------------------------------------------START
+		timp_start = std::chrono::high_resolution_clock::now();
 
-	std::vector< std::vector<long long> > Rezultat = produsCSR(Matrice1, Matrice2);
+		//CITIRE NUMERE MARI
+		FILE *file1, *file2;
+		errno_t err1 = fopen_s(&file1, "C:\\Users\\pati\\Desktop\\lab2\\numar1.txt", "r");
+		errno_t err2 = fopen_s(&file2, "C:\\Users\\pati\\Desktop\\lab2\\numar2.txt", "r");
 
-	auto end = std::chrono::high_resolution_clock::now();
-	RezultatFile << "Timp: " << CAST(end - start).count() << std::endl;
-	////--------------------------------------------------------------------
+		//lungime fisier1
+		fseek(file1, 0, SEEK_END);
+		int len1 = ftell(file1);
+		fseek(file1, 0, SEEK_SET);
 
-	scriereMatrice(RezultatFile, Rezultat);
+		//lungime fisier2
+		fseek(file2, 0, SEEK_END);
+		int len2 = ftell(file2);
+		fseek(file2, 0, SEEK_SET);
 
-	Matrice1File.close();
-	Matrice2File.close();
-	RezultatFile.close();
+		//citire numar 1
+		number1 = (char*)malloc(sizeof(char)*len1 + 1);
+		for (int i = len1 - 1; i >= 0; i--)
+		{
+			fscanf_s(file1, "%c", number1 + i);
+		}
+		number1[len1] = '\0';
+
+		//citire numar 2
+		number2 = (char*)malloc(sizeof(char)*len2 + 1);
+		for (int i = len2 - 1; i >= 0; i--)
+		{
+			fscanf_s(file2, "%c", number2 + i);
+		}
+		number2[len2] = '\0';
+
+		//number1 - numarul mai mic
+		if (len1 > len2) {
+			char *aux = number1;
+			number1 = number2;
+			number2 = aux;
+
+			int auxx = len1;
+			len1 = len2;
+			len2 = auxx;
+		}
+
+		rem = len1 % nrprocese;
+		sendcounts = (int*)malloc(sizeof(int)*len1);
+		displs = (int*)malloc(sizeof(int)*len1);
+
+		// calculate send counts and displacements
+		//pentru procesul 0
+		len_segm = len1 / nrprocese;
+		sendcounts[0] = len_segm;
+		if (rem > 0) {
+			sendcounts[0]++;
+			len_segm++;
+			rem--;
+		}
+
+		//cout << "RANK " << rank << " : len_segm=" << len_segm << endl;
+
+		displs[0] = sum;
+		sum += sendcounts[0];
+
+		//pentru restul proceselor
+		int len_segm2 = len1 / nrprocese;
+		for (int i = 1; i < nrprocese; i++) {
+			sendcounts[i] = len_segm2;
+
+			if (rem > 0) {
+				sendcounts[i]++;
+				rem--;
+			}
+
+			//trimitem fiecarui proces lungimea segmentului corespunzator
+			MPI_Send(&sendcounts[i], 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+
+			displs[i] = sum;
+			sum += sendcounts[i];
+		}
+		/*for (int i = 0; i < nrprocese; i++) {
+			printf("sendcounts[%d] = %d\tdispls[%d] = %d\n", i, sendcounts[i], i, displs[i]);
+		}*/
+	}
+	else {
+		MPI_Recv(&len_segm, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+		//cout << "RANK " << rank << " : len_segm=" << len_segm << endl;
+	}
+
+	//alocam spatiu si pentru \0
+	segment_number1 = (char*)malloc(sizeof(char)*(len_segm + 1));
+	segment_number2 = (char*)malloc(sizeof(char)*(len_segm + 1));
+
+	MPI_Scatterv(number1, sendcounts, displs, MPI_CHAR, segment_number1, len_segm + 1, MPI_CHAR, 0, MPI_COMM_WORLD);
+	MPI_Scatterv(number2, sendcounts, displs, MPI_CHAR, segment_number2, len_segm + 1, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+	segment_number1[len_segm] = '\0';
+	segment_number2[len_segm] = '\0';
+
+	//cout << "RANK " << rank << " : " << segment_number1 <<" "<<segment_number2 << endl;
+
+	if (rank == 0) {
+		char *summ;
+		int len1 = strlen(number1), len2 = strlen(number2);
+
+		summ = (char*)malloc(sizeof(char)*len_segm);
+
+		//Calcul suma partiale pentru procesul 0
+		int temporalCarry = 0;
+		for (int i = 0; i < len_segm; i++) {
+			int temporalSum = segment_number1[i] - '0' + segment_number2[i] - '0' + temporalCarry;
+			summ[i] = (temporalSum % 10) + '0';
+			//cout << "RANK " << rank << " " << segment_number1[i] - '0' << "+" << segment_number2[i] - '0' << "+" << temporalCarry << " = " << temporalSum << " => "<< summ[i]<< endl;
+			temporalCarry = temporalSum / 10;
+		}
+		if (temporalCarry != 0) {
+			summ[len_segm] = (char)malloc(sizeof(char));
+			summ[len_segm] = temporalCarry + '0';
+			//cout << "RANK " << rank << " carry=" << summ[len_segm] << endl;
+			len_segm++;
+		}
+
+		//caracterul '\0'
+		summ[len_segm] = (char)malloc(sizeof(char));
+		summ[len_segm] = '\0';
+
+		//cout << "RANK " << rank << " suma=" << summ << endl;
+
+		//Pregatim suma finala (len2 + 1 -> rez poate avea maxim o cifra in plus fata de cel mai mare numar )
+		vector<char> summ_final;
+		for (int i = 0; i < len2 + 1; i++)
+			summ_final.push_back('0');
+
+		//Punem suma din procesul 0 in suma finala
+		for (int i = 0; i < len_segm; i++)
+			summ_final[i] = summ[i];
+
+		/*cout << "RANK " << rank << " suma finala=";
+		for (int i = 0; i < len2 + 1; i++)
+			cout << summ_final[i];
+		cout << endl;*/
+
+		//SUMELE PRIMITE
+		char **summ_primit = (char**)malloc(sizeof(char)*nrprocese);
+		int recv_count_max = len1 / nrprocese + 1;	//lungimea segm maxim trimisa din pr 0
+		recv_count_max += 1 + 1 + 1;					//o sa primim in plus: rank, carry, '\0'
+		recv_count_max = recv_count_max * 2;
+
+		//Primim sumele intr-o ordine oarecare
+		for (int i = 1; i < nrprocese; i++) {
+			summ_primit[i] = (char*)malloc(sizeof(char)*recv_count_max);
+			MPI_Recv(summ_primit[i], recv_count_max, MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+			//cout << "FROM RANK " << summ_primit[i][0] << " : " << summ_primit[i] << endl;
+		}
+
+		int start;
+		//Concatenare sume primite in suma finala
+		for (int i = 1; i < nrprocese; i++) {
+			int rank_sender = summ_primit[i][0] - '0';
+
+			//Calculam pozitia de start
+			if (rank_sender < len1 % nrprocese)
+				start = rank_sender * (len1 / nrprocese + 1);
+			else
+				start = len1 % nrprocese + len1 / nrprocese * rank_sender;
+
+			int j = 1, temporalCarry = 0;
+			while (summ_primit[i][j] != '\0') {
+				int temporalSum = summ_final[start] - '0' + summ_primit[i][j] - '0' + temporalCarry;
+				summ_final[start] = (temporalSum % 10) + '0';
+				temporalCarry = temporalSum / 10;
+				start++;
+				j++;
+			}
+			if (temporalCarry != 0) {
+				int temporalSum = summ_final[start] - '0' + temporalCarry;
+				summ_final[start] = temporalSum + '0';
+				len_segm++;
+			}
+		}
+
+		/*cout << "RANK " << rank << " suma finala=";
+		for (int i = 0; i < len2 + 1; i++)
+			cout << summ_final[i];
+		cout << endl;*/
+
+		//Concatenare cu restul din numarul mare
+		start = len1 % (nrprocese)+len1 / nrprocese * nrprocese;
+		temporalCarry = 0;
+		for (int i = start; i < len2; i++) {
+			//cout << "pozitia: " << i << " cifra: " << number2[i] << endl;
+			int temporalSum = summ_final[i] - '0' + number2[i] - '0' + temporalCarry;
+			summ_final[i] = (temporalSum % 10) + '0';
+			temporalCarry = temporalSum / 10;
+		}
+		if (temporalCarry != 0) {
+			int temporalSum = summ_final[len2] - '0' + temporalCarry;
+			summ_final[len2] = temporalSum + '0';
+		}
+
+		//-----------------------------------------------------------------------------------END
+		timp_end = std::chrono::high_resolution_clock::now();
+		cout << "TIMP: " << CAST(timp_end - timp_start).count() << endl;
+
+		ofstream g("C:\\Users\\pati\\Desktop\\lab2\\rezultat.txt");
+
+		cout << endl;
+		if (summ_final[len2] == '0')	len2--;
+		for (int i = len2; i >= 0; i--) {
+			cout << summ_final[i];
+			g << summ_final[i];
+		}
+	}
+	else {
+		char *summ;
+		summ = (char*)malloc(sizeof(char)*len_segm);
+
+		//Punem rankul pe prima pozitie
+		summ[0] = rank + '0';
+		summ[len_segm] = (char)malloc(sizeof(char));
+		len_segm++;
+
+		//Calcul suma partiala
+		int temporalCarry = 0;
+		for (int i = 0; i < len_segm - 1; i++) {
+			int temporalSum = segment_number1[i] - '0' + segment_number2[i] - '0' + temporalCarry;
+			summ[i + 1] = (temporalSum % 10) + '0';
+			//cout << "RANK " << rank << " " << segment_number1[i] - '0' << "+" << segment_number2[i] - '0' << "+" << temporalCarry << " = " << temporalSum << " => "<< summ[i]<< endl;
+			temporalCarry = temporalSum / 10;
+		}
+		if (temporalCarry != 0) {
+			summ[len_segm] = (char)malloc(sizeof(char));
+			summ[len_segm] = temporalCarry + '0';
+			//cout << "RANK " << rank << " carry=" << summ[len_segm] << endl;
+			len_segm++;
+		}
+
+		//caracterul '\0'
+		summ[len_segm] = (char)malloc(sizeof(char));
+		summ[len_segm] = '\0';
+
+		//cout << "RANK " << rank << " rank+suma=" << summ << endl;
+
+		//Trimitem suma partiala procesului 0 impreuna cu '\0' ca sa stim cand se termina sirul in pr 0
+		MPI_Send(summ, len_segm + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+	}
+
+
+	//cout << endl << "Finalize " << rank <<"...";
+	MPI_Finalize();
 }
+
+//MPI_Wait
+//MPI_Test
+//MPI_Irecv
---
+++ mpi-produs/secvential-produs.cpp
@@ -0,0 +1,177 @@
+#include "pch.h"
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <chrono>
+
+#define CAST std::chrono::duration_cast<std::chrono::nanoseconds>
+
+struct MatriceCSR {
+	long long nr_linii, nr_coloane;
+	std::vector<long long> V, COL_INDEX, ROW_INDEX;
+} Matrice1, Matrice2;
+
+void citireMatrice(std::ifstream &MatriceFile, MatriceCSR &Matrice) {
+	MatriceFile >> Matrice.nr_linii;
+	MatriceFile >> Matrice.nr_coloane;
+
+	Matrice.ROW_INDEX.push_back(0);
+
+	for (long long i = 0; i < Matrice.nr_linii; i++) {
+		for (long long j = 0; j < Matrice.nr_coloane; j++) {
+			long long x;
+			MatriceFile >> x;
+			if (x != 0) {
+				Matrice.V.push_back(x);
+				Matrice.COL_INDEX.push_back(j);
+			}
+		}
+		Matrice.ROW_INDEX.push_back(Matrice.V.size());
+	}
+}
+
+void scriereMatrice(std::ofstream &MatriceFile, std::vector< std::vector<long long> > &Matrice) {
+	MatriceFile << Matrice1.nr_linii << " " << Matrice2.nr_linii << std::endl;
+	for (long long i = 0; i < Matrice1.nr_linii; i++) {
+		for (long long j = 0; j < Matrice2.nr_linii; j++) {
+			MatriceFile << Matrice[i][j] << " ";
+		}
+		MatriceFile << std::endl;
+	}
+}
+
+//calculeaza transpusa in format CSR a unei matrice in format CSR
+MatriceCSR transpusaCSR(MatriceCSR &Matrice) {
+	MatriceCSR Rezultat;
+
+	Rezultat.nr_linii = Matrice.nr_coloane;
+	Rezultat.nr_coloane = Matrice.nr_linii;
+	Rezultat.ROW_INDEX.push_back(0);
+
+	for (long long j = 0; j < Matrice.nr_coloane; j++) {
+		for (long long i = 0; i < Matrice.nr_coloane; i++) {
+			if (Matrice.COL_INDEX.at(i) == j) {
+				Rezultat.V.push_back(Matrice.V.at(i));
+
+				long long k = 0;
+				while (Matrice.ROW_INDEX.at(k) <= i)
+					k++;
+				Rezultat.COL_INDEX.push_back(k - 1);
+			}
+		}
+		Rezultat.ROW_INDEX.push_back(Rezultat.V.size());
+	}
+
+	return Rezultat;
+}
+
+//returneaza subsecventa din vector incepand de la indexul x si terminand la indexul y-1
+std::vector<long long> subsecventa(std::vector<long long> vector, long long x, long long y) {
+	std::vector<long long>::const_iterator first = vector.begin() + x;
+	std::vector<long long>::const_iterator last = vector.begin() + y;
+	std::vector<long long> rezultat(first, last);
+	return rezultat;
+}
+
+//returneaza elementele comune a doi vectori (ca valoare) si indecsii corespunzatori valorilor comune pentru fiecare dintre cei doi vectori (idx1 si idx2)
+std::vector<long long> intersectie(std::vector<long long> vector1, std::vector<long long> vector2, std::vector<long long> &idx1, std::vector<long long> &idx2) {
+	std::vector<long long> rezultat;
+	long long curent1 = 0, curent2 = 0;
+
+	while (curent1 < vector1.size() && curent2 < vector2.size()) {
+		long long left = vector1[curent1];
+		long long right = vector2[curent2];
+
+		if (left == right) {
+			rezultat.push_back(right);
+			idx1.push_back(curent1);
+			idx2.push_back(curent2);
+
+			while (curent1 < vector1.size() && vector1[curent1] == left)
+				curent1++;
+			while (curent2 < vector2.size() && vector2[curent2] == right)
+				curent2++;
+			continue;
+		}
+
+		if (left < right) {
+			while (curent1 < vector1.size() && vector1[curent1] == left)
+				curent1++;
+		}
+		else 
+			while (curent2 < vector2.size() && vector2[curent2] == right)
+				curent2++;
+	}
+	return rezultat;
+}
+
+//std::vector<long long> produsCSR_Vector_Matrice(std::vector<long long> &Vector, MatriceCSR &Matrice2) {
+//
+//}
+
+std::vector< std::vector<long long> > produsCSR(MatriceCSR &Matrice1, MatriceCSR &Matrice2) {
+
+	//Pregatire matrice rezultat
+	std::vector< std::vector<long long> > A;
+	A.resize(Matrice1.nr_linii);
+	for (long long i = 0; i < Matrice1.nr_linii; ++i)
+	{
+		A[i].resize(Matrice2.nr_linii);
+	}
+
+	//Calculare produs
+	long long linii_matrice1 = Matrice1.nr_linii;
+	long long linii_matrice2 = Matrice2.nr_linii;
+	for (long long linie_curenta_matrice1 = 1; linie_curenta_matrice1 <= linii_matrice1; linie_curenta_matrice1++) {
+
+		std::vector<long long> COL_INDEX_1 = subsecventa(Matrice1.COL_INDEX, Matrice1.ROW_INDEX[linie_curenta_matrice1 - 1], Matrice1.ROW_INDEX[linie_curenta_matrice1]);
+		std::vector<long long> V_1 = subsecventa(Matrice1.V, Matrice1.ROW_INDEX[linie_curenta_matrice1 - 1], Matrice1.ROW_INDEX[linie_curenta_matrice1]);
+
+		for (long long linie_curenta_matrice2 = 1; linie_curenta_matrice2 <= linii_matrice2; linie_curenta_matrice2++) {
+			A[linie_curenta_matrice1-1][linie_curenta_matrice2-1] = 0;
+			
+			std::vector<long long> COL_INDEX_2 = subsecventa(Matrice2.COL_INDEX, Matrice2.ROW_INDEX[linie_curenta_matrice2 - 1], Matrice2.ROW_INDEX[linie_curenta_matrice2]);
+			std::vector<long long> V_2 = subsecventa(Matrice2.V, Matrice2.ROW_INDEX[linie_curenta_matrice2 - 1], Matrice2.ROW_INDEX[linie_curenta_matrice2]);
+
+			std::vector<long long> idx1;
+			std::vector<long long> idx2;
+
+			std::vector<long long> COL_INDEX_BOTH = intersectie(COL_INDEX_1, COL_INDEX_2, idx1, idx2);
+			for (long long k = 0; k < COL_INDEX_BOTH.size(); k++) {
+				A[linie_curenta_matrice1-1][linie_curenta_matrice2-1] += V_1[idx1[k]] * V_2[idx2[k]];
+			}
+		}
+	}
+	return A;
+}
+
+int main()
+{
+	std::string prefix_fisier = "C:\\Users\\pati\\Desktop\\UNIV\\SEM6\\PP\\proiect\\data\\generat0";
+
+	std::ifstream Matrice1File(prefix_fisier + "A.txt");
+	std::ifstream Matrice2File(prefix_fisier + "B.txt");
+	std::ofstream RezultatFile(prefix_fisier + "C.txt");
+
+	citireMatrice(Matrice1File, Matrice1);
+	citireMatrice(Matrice2File, Matrice2);
+
+	Matrice2 = transpusaCSR(Matrice2);
+
+	////------------------------------------------------------------------
+	auto start = std::chrono::high_resolution_clock::now();
+
+	std::vector< std::vector<long long> > Rezultat = produsCSR(Matrice1, Matrice2);
+
+	auto end = std::chrono::high_resolution_clock::now();
+	RezultatFile << "Timp: " << CAST(end - start).count() << std::endl;
+	////--------------------------------------------------------------------
+
+	scriereMatrice(RezultatFile, Rezultat);
+
+	Matrice1File.close();
+	Matrice2File.close();
+	RezultatFile.close();
+}
